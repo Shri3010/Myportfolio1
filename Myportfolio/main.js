3957e21eaab4062658d4a3efa41be2b9a22a66db
Shri3010 <Shribalajikarthik@gmail.com>
2024-06-13 22:52:24 +0530

    stage

--- Myportfolio/main.js
+++ Myportfolio/main.js
@@ -5,3 +5,28 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+document.getElementById('contactForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    console.log(email + message);
+
+    const response = await fetch('https://eykrj28u10.execute-api.us-east-1.amazonaws.com/prod', {
+        method: 'POST',
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, message })
+    });
+
+    if (response.ok) {
+        console.log('Message sent successfully!');
+    } else {
+        console.log('Failed to send message.');
+    }
+});
